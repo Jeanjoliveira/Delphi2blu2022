9099719e724c23a6f34f80f4e205bfd4a0435351
FURB\craymundo <carlosfelipeor@gmail.com>
2022-10-04 19:34:52 -0300

    Alterações realizadas por Carlos Felipe

--- Exercício 35/Unit2.pas
+++ Exercício 35/Unit2.pas
@@ -7,6 +7,8 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
+  TEnumCliente = (tcResidencia=1, tcComercio, tcIndustria,tcRural);
+
   TForm2 = class(TForm)
     memolista: TMemo;
     btmcalcular: TButton;
@@ -15,6 +17,7 @@ type
     procedure btmcalcularClick(Sender: TObject);
   private
     { Private declarations }
+    procedure ContaDeLuz;
   public
     { Public declarations }
   end;
@@ -26,30 +29,44 @@ implementation
 
 {$R *.dfm}
 
-procedure TForm2.btmcalcularClick(Sender: TObject);
+procedure TForm2.ContaDeLuz;
   var
+    xvalor, xdesconto : Currency;
     xconsumo,  xresidencia, xcomercio, xindustria : double;
-     xcliente : string;
+    xcliente : Integer;
 
 
 begin
-  xcliente := inputbox ('Informar','Informe se o cliente é residencia, comercio ou industria ','');
-  xconsumo:= strtocurr(inputbox ('Informar','Informe o valor de consumo',' '));
+  xcliente := StrToIntdef(inputbox ('Informar',
+  'Tipo de cliente: 1-Residencia, 2-Comercio, 3-Industria ou 4-Rural',''),1);
+  xconsumo:= strtocurr(inputbox ('Informar',
+                                 'Informe o valor de consumo em kWh ',''));
+  xdesconto := strtocurr(inputbox ('Informar', 'Valor de desconto em R$','0'));
 
-  if xcliente = 'residencia' then
-     xconsumo := xconsumo * 1.60
 
-  else if xcliente = 'comercio'  then
-          xconsumo := xconsumo * 1.48
 
-  else if  xcliente = 'industria'  then
-           xconsumo := xconsumo  *1.29 ;
+  case TEnumCliente(xcliente) of
+    tcResidencia:
+      xvalor := xconsumo * 0.60;
+    tcComercio:
+      xvalor := xconsumo * 0.48;
+    tcIndustria:
+      xvalor := xconsumo  *1.29;
+    tcRural:
+      xvalor := xconsumo  * 2.18 ;
+  end;
 
-    memolista.lines.add ('Seu valor de consumo é de ' +(floattostr(xconsumo)+ ' reais'));
+  xvalor := xvalor - xdesconto;
+
+  memolista.lines.add ('Seu valor de consumo é de R$'
+                           +(FormatCurr('####0.00',xvalor)));
 
   end;
 
 
-
+procedure TForm2.btmcalcularClick(Sender: TObject);
+begin
+  Self.ContaDeLuz;
+end;
 
 end.
